f5709d60238351e758e8ec4897c5c50f24768edf
Gretar Mar <grellinn@gmail.com>
2016-10-29 21:52:49 +0000

    Log message changed

--- src/main/java/is/gens/tictactoe/TicTacToe.java
+++ src/main/java/is/gens/tictactoe/TicTacToe.java
@@ -7,6 +7,6 @@ public class TicTacToe {
 		Board board = new Board();    
 		board.displayBoard(table);
 		
-		System.out.println("Everything is working now!");
+		System.out.println("Whatta hell maaaður!");
     }
 }
